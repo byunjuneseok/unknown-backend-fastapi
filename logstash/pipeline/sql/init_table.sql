f8ddde245b852507f7bd02dfd7c7d00b89ca8e9c
byunjuneseok <byun@indentcorp.com>
2021-11-09 00:19:57 +0900

    Remove ELK Stack and database from project

--- logstash/pipeline/sql/init_table.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM agd.store
-ORDER BY id
--- logstash/pipeline/sql/sync_table.sql
+++
@@ -1,3 +0,0 @@
-SELECT *, update_date_time AS ts FROM agd.store
-WHERE update_date_time > :sql_last_value AND update_date_time < NOW()
-ORDER BY id
